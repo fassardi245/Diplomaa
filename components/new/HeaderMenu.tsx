b632f9cd86303a44dff1d44339ad2a2ca6d17fc8
Walter Toledo <toleeewalter@gmail.com>
2025-11-28 12:31:26 -0300

    Header Cliente arreglado

--- components/new/HeaderMenu.tsx
+++ components/new/HeaderMenu.tsx
@@ -8,56 +8,47 @@ const HeaderMenu = ({ categories }: { categories: CATEGORIES_QUERYResult }) => {
 
   return (
     <div className="hidden md:inline-flex w-1/3 items-center gap-5 text-sm capitalize font-semibold text-lightColor">
+      
+      {/* HOME */}
       <Link
         href={"/"}
         className={`hover:text-darkColor hoverEffect relative group ${pathname === "/" && "text-darkColor"}`}
       >
         Home
-        <span
-          className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:left-0 ${
-            pathname === "/" && "w-1/2"
-          }`}
-        />
-        <span
-          className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:right-0 ${
-            pathname === "/" && "w-1/2"
-          }`}
-        />
+        <span className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:left-0 ${pathname === "/" && "w-1/2"}`} />
+        <span className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:right-0 ${pathname === "/" && "w-1/2"}`} />
       </Link>
+
+      {/* CATEGORÍAS (Dinámicas) */}
       {categories?.map((category: Category) => (
         <Link
           key={category?._id}
           href={`/category/${category?.slug?.current}`}
-          className={`hover:text-darkColor hoverEffect relative group ${pathname === `/category/${category?.slug?.current}` && "text-darkColor"}`}
+          className={`hover:text-darkColor hoverEffect relative group ${
+            pathname === `/category/${category?.slug?.current}` && "text-darkColor"
+          }`}
         >
           {category?.title}
-          <span
-            className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:left-0 ${
+          <span className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:left-0 ${
               pathname === `/category/${category?.slug?.current}` && "w-1/2"
             }`}
           />
-          <span
-            className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:right-0 ${
+          <span className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:right-0 ${
               pathname === `/category/${category?.slug?.current}` && "w-1/2"
             }`}
           />
         </Link>
       ))}
+
+      {/* SHOP (Corregido) */}
       <Link
         href={"/shop"}
-        className={`hover:text-darkColor hoverEffect relative group ${pathname === "/" && "text-darkColor"}`}
+        // CORRECCIÓN AQUÍ: Antes decía pathname === "/"
+        className={`hover:text-darkColor hoverEffect relative group ${pathname === "/shop" && "text-darkColor"}`}
       >
         Shop
-        <span
-          className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:left-0 ${
-            pathname === "/shop" && "w-1/2"
-          }`}
-        />
-        <span
-          className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:right-0 ${
-            pathname === "/shop" && "w-1/2"
-          }`}
-        />
+        <span className={`absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:left-0 ${pathname === "/shop" && "w-1/2"}`} />
+        <span className={`absolute -bottom-0.5 right-1/2 w-0 h-0.5 bg-darkColor transition-all duration-300 group-hover:w-1/2 group-hover:right-0 ${pathname === "/shop" && "w-1/2"}`} />
       </Link>
     </div>
   );
